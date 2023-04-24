7e3a99a1c7e527915b51c150ae6de8fac94a7e6e
SylviaRatemo <ratemosylvia@gmail.com>
2023-04-24 10:06:45 -0700

    Insetion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+
+/**
+ * move_to_head - Get head of list
+ * @node: a node in list
+ * Return: the head of list
+ */
+
+listint_t *move_to_head(listint_t *node)
+{
+	while (node->prev)
+		node = node->prev;
+	return (node);
+}
+
+/**
+ * insertion_sort_list - doubly linked list, ascending, inseriton sort
+ * @list: the doubly linked list
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp, *buff, *ptr;
+
+	if (list == NULL)
+		return;
+	
+	temp = move_to_head(*list);
+
+	for (temp = temp->next; temp;)
+	{
+		buff = temp->next;
+		while (temp->prev && temp->n < temp->prev->n)
+		{
+			ptr = temp->prev;
+			ptr->next = temp->next;
+			temp->prev = ptr->prev;
+			ptr->prev = temp;
+			temp->next = ptr;
+			if (ptr->next)
+				ptr->next->prev = ptr;
+			if (temp->prev)
+				temp->prev->next = temp;
+			print_list(move_to_head(*list));
+		}
+		temp = buff;
+	}
+	*list = move_to_head(*list);
+}
